11e0f2bed3f53a62269348112ea7711a99639ea2
ANTONIO ESPÍN <1902812@alu.murciaeduca.es>
2023-12-27 21:14:34 +0100

    9
    9

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
 import '../../node_modules/bootstrap';
 import "../../node_modules/@fortawesome/fontawesome-free/js/all";
-console.log("plantilla de Juan que se lo tiene muy creido");
+import { getSlider } from '../../node_modules/simple-slider';
+ 
+getSlider();
